51adf0a91ac1a6d134b44681f4107d6e7acb228a
Brian Cappello <briancappello@gmail.com>
2016-10-06 00:07:39 -0400

    verify d3 available on window object

--- src/techan.js
+++ src/techan.js
@@ -2,8 +2,8 @@
 
 var _d3;
 
-// If running in browser (window !undefined), assume d3 available
-if('undefined' != typeof window) _d3 = window.d3;
+// If running in browser (window !undefined) and d3 available
+if('undefined' != typeof window && window.d3) _d3 = window.d3;
 else if('object' == typeof module) _d3 = require('d3'); // else we're in the only other supported mode: v8/node
 else throw "Unsupported runtime environment: Could not find d3. Ensure defined globally on window, or available as dependency.";
 
